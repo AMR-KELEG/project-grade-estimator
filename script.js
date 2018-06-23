16651cca77345de2f91a7a040b6f5ac60b30faf9
Amr Keleg <amr_mohamed@live.com>
2018-06-23 21:07:54 +0200

    Add a selected attribute to each answer

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 function updateScore(elem){
-	var x = elem.parentElement;
-	// alert(x.children.length);
-	var c = x.children;
+	var parent_question = elem.parentElement;
+	var c = parent_question.children;
 	var i;
+	var selected_item = null;
+	var score_item = null;
 	for (i = 0; i < c.length; i++) {
-		if (c[i].id == 'score')
-		{
+		if (c[i].id == 'score') {
+			score_item = c[i];
+			break;
+		}
+	}
+	for (i = 0; i < c.length; i++) {
+		if (c[i].hasAttribute('selected') &&
+			c[i].getAttribute('selected') == 'True') {
+			c[i].style.background = '#FFFFFF';
+			selected_item = c[i];
+			break;
+		}
+	}
+			/*
 			var delta = elem.getAttribute('score') - parseInt(c[i].getAttribute('score'));
 			c[i].innerText = elem.getAttribute('score');
 			c[i].setAttribute('score', elem.getAttribute('score'));
-			var total_score = document.getElementById('total_score');
-			var cur_total = parseInt(total_score.getAttribute('score'));
 			cur_total += delta;
 			total_score.setAttribute('score', cur_total);
 			total_score.innerText = cur_total;
-		}
-		c[i].style.backgroundColor = '#D3D3D3';
+			*/
+	var total_score = document.getElementById('total_score');
+	var cur_total = parseInt(total_score.getAttribute('score'));
+	if (selected_item === elem)
+	{
+		elem.setAttribute('selected', 'False');
+		elem.style.background = '#FFFFFF'
+		cur_total = 0;
 	}
-	elem.style.background = '#9370DB';
+
+	else if (selected_item === null) {
+		elem.setAttribute('selected', 'True');
+		elem.style.background = '#9370DB';
+		cur_total += parseInt(elem.getAttribute('score'));
+	}
+	else
+	{
+		elem.setAttribute('selected', 'True');
+		selected_item.setAttribute('selected', 'False');
+		elem.style.background = '#9370DB';
+		cur_total += parseInt(elem.getAttribute('score')) -
+					parseInt(selected_item.getAttribute('score'));
+	}
+	total_score.setAttribute('score', cur_total);
+	total_score.innerText = cur_total;
+	score_item.innerText = cur_total;
 }
